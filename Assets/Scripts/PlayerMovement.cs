bfdd43ce3d43805062dc522232d5801eb7d83763
David Le <ledaodavid@gmail.com>
2021-04-30 13:30:26 +0200

    Anim

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Animator ani;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
