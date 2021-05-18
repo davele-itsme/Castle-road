0c20cd7d2a4a25f39c0a08b1066eac5d7e92af28
David Le <ledaodavid@gmail.com>
2021-05-18 03:27:38 +0200

    Save player prefs

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    [SerializeField] private Slider soundSlider;
+    [SerializeField] private Slider musicSlider;
 
-    public void Start()
+    public void OnEnable()
     {
-        
+        var soundVolume = PlayerPrefs.GetFloat("SoundVolume");
+        var musicVolume = PlayerPrefs.GetFloat("MusicVolume");
+        if (soundVolume != 0)
+        {
+            SetSound(soundVolume);
+        }
+
+        if (musicVolume != 0)
+        {
+            SetMusic(musicVolume);
+        }
     }
 
     public void PlaySound()
@@ -17,16 +30,15 @@ public class SettingsMenu : MonoBehaviour
     
     public void SetMusic(float volume)
     {
-        audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
-        
+        var fixedVolume = Mathf.Log10(volume) * 20;
+        audioMixer.SetFloat("MusicVolume", fixedVolume);
+        PlayerPrefs.SetFloat("MusicVolume", fixedVolume);
     }
 
     public void SetSound(float volume)
     {
-        audioMixer.SetFloat("SoundVolume", Mathf.Log10(volume) * 20);
+        var fixedVolume = Mathf.Log10(volume) * 20;
+        audioMixer.SetFloat("SoundVolume", fixedVolume);
+        PlayerPrefs.SetFloat("SoundVolume", fixedVolume);
     }
-    
-    
-
- 
 }
