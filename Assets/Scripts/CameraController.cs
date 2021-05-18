6637521e8a45d4a96be27714598bead3ef4bdc7d
David Le <ledaodavid@gmail.com>
2021-05-18 04:04:56 +0200

    Camera stop following

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,13 +1,32 @@
+
+using System.Collections;
+using Player;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform playerTransform;
     private Vector3 _shouldFollow;
+    private bool _follow = true;
 
-    private void Update()
+    private void Start()
     {
-        _shouldFollow = Vector3.Lerp(transform.position, playerTransform.position, Time.deltaTime);
-        transform.position = _shouldFollow;
+        StartCoroutine(FollowPlayer());
+        PlayerDeath.PlayerDied += StopFollowing;
+    }
+
+    private void StopFollowing()
+    {
+        _follow = false;
+    }
+
+    private IEnumerator FollowPlayer()
+    {
+        do
+        {
+            _shouldFollow = Vector3.Lerp(transform.position, playerTransform.position, Time.deltaTime);
+            transform.position = _shouldFollow;
+            yield return null;
+        } while (_follow);
     }
 }
--- Assets/Scripts/Player/PlayerDeath.cs
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -4,12 +4,20 @@ namespace Player
 {
     public class PlayerDeath : MonoBehaviour
     {
+        public delegate void PlDeath();
+        public static event PlDeath PlayerDied;
+        
         [SerializeField] private GameObject gameOverMenu;
         [SerializeField] private GameObject pauseButton;
+
         private void OnDestroy()
         {
             gameOverMenu.SetActive(true);
             pauseButton.SetActive(false);
+            if (PlayerDied != null)
+            {
+                PlayerDied();
+            }
         }
     }
 }
